73724b77a2dc979280b6b81cb8b9704e5ee9bb72
Armand <armandbali23@gmail.com>
2025-06-12 10:41:36 +0200

    fix: back to existing pickle dataset

--- src/job_finder/datasets.py
+++ src/job_finder/datasets.py
@@ -2,51 +2,62 @@ from kedro.io import AbstractVersionedDataset
 import pickle
 import fsspec
 from kedro.io.core import get_filepath_str, get_protocol_and_path
-from pathlib import Path
+from pathlib import PurePosixPath
+from typing import Any
 
 
 class OptionalPickleDataset(AbstractVersionedDataset):
-    """Custom dataset that prevents an error to raise if there is no model on s3 yet."""
+    """Custom PickleDataset that does not fail if file is missing."""
 
     def __init__(
-        self, filepath, version=None, credentials=None, load_args=None, save_args=None
+        self,
+        filepath: str,
+        version=None,
+        credentials: dict = None,
+        load_args: dict = None,
+        save_args: dict = None,
     ):
-        self._filepath = filepath
-        self._credentials = credentials or {}
+        # Kedro internally expects a Path-like object here
+        self._filepath = PurePosixPath(filepath)
         self._protocol, self._path = get_protocol_and_path(filepath, version)
+        self._credentials = credentials or {}
         self._fs = fsspec.filesystem(self._protocol, **self._credentials)
 
         self._load_args = load_args or {}
         self._save_args = save_args or {}
 
-        super().__init__(filepath=filepath, version=version)
+        super().__init__(filepath=self._filepath, version=version)
 
-    def _load(self):
-        load_path = get_filepath_str(self._get_load_path(), self._protocol)
+    def _load(self) -> Any:
+        load_path = self._get_load_path()
         try:
             with self._fs.open(load_path, mode="rb") as f:
                 return pickle.load(f, **self._load_args)
-        except FileNotFoundError:
+        except (FileNotFoundError, EOFError):
             return None
 
-    def _save(self, data) -> None:
-        save_path = get_filepath_str(self._get_save_path(), self._protocol)
+    def _save(self, data: Any) -> None:
+        save_path = self._get_save_path()
         with self._fs.open(save_path, mode="wb") as f:
             pickle.dump(data, f, **self._save_args)
 
-    def _get_load_path(self) -> Path:
+    def _get_load_path(self) -> str:
         if self._version and self._version.load:
-            return Path(self._path) / self._version.load
-        return Path(self._path)
+            return f"{self._path}/{self._version.load}/{self._filepath.name}"
+        return str(self._path)
 
-    def _get_save_path(self) -> Path:
+    def _get_save_path(self) -> str:
         if self._version and self._version.save:
-            return Path(self._path) / self._version.save
-        return Path(self._path)
+            return f"{self._path}/{self._version.save}/{self._filepath.name}"
+        return str(self._path)
 
-    def _describe(self):
+    def _get_versioned_path(self, version: str) -> PurePosixPath:
+        # Required for Kedro to compute versioned save/load directories
+        return PurePosixPath(self._filepath) / version / self._filepath.name
+
+    def _describe(self) -> dict:
         return {
-            "filepath": self._filepath,
+            "filepath": str(self._filepath),
             "version": self._version,
             "protocol": self._protocol,
         }
