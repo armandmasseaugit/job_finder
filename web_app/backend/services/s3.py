1478c1fa3f6e200cea337c4b3c7f98cd1edf0330
Armand <armandbali23@gmail.com>
2025-07-06 16:47:51 +0200

    feat: add redis cache to back end

--- web_app/backend/services/s3.py
+++ web_app/backend/services/s3.py
@@ -4,10 +4,12 @@ import numpy as np
 import pandas as pd
 import boto3
 import json
+import redis
 from kedro.config import OmegaConfigLoader
 
-
-# TODO: replace credentials source from local to kubernetes secrets
+# -------------------------
+# AWS S3 CONFIG
+# -------------------------
 creds = (
     OmegaConfigLoader(
         conf_source="conf/",
@@ -26,13 +28,32 @@ s3 = boto3.client(
 
 BUCKET = "wttj-scraping"
 
+# -------------------------
+# REDIS CONFIG
+# -------------------------
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    db=0,
+    decode_responses=True,
+)
+
+CACHE_TTL = 300  # secondes (5 minutes)
+
+
 def get_offers():
     try:
+        cache_key = "offers"
+        if redis_client.exists(cache_key):
+            return json.loads(redis_client.get(cache_key))
         obj = s3.get_object(Bucket=BUCKET, Key="wttj_jobs.xlsx")
         buffer = BytesIO(obj["Body"].read())
         df = pd.read_excel(buffer)
         df = df.replace({np.nan: None, np.inf: None, -np.inf: None})
-        return df.to_dict(orient="records")
+        offers = df.to_dict(orient="records")
+
+        redis_client.setex(cache_key, CACHE_TTL, json.dumps(offers))
+        return offers
     except s3.exceptions.NoSuchKey:
         return []
     except Exception:
@@ -41,8 +62,15 @@ def get_offers():
 
 def get_likes():
     try:
+        cache_key = "job_likes"
+        if redis_client.exists(cache_key):
+            return json.loads(redis_client.get(cache_key))
+
         obj = s3.get_object(Bucket=BUCKET, Key="job_likes.json")
-        return json.loads(obj["Body"].read().decode("utf-8"))
+        likes = json.loads(obj["Body"].read().decode("utf-8"))
+
+        redis_client.setex(cache_key, CACHE_TTL, json.dumps(likes))
+        return likes
     except s3.exceptions.NoSuchKey:
         return {}
     except Exception:
@@ -51,8 +79,15 @@ def get_likes():
 
 def get_relevance():
     try:
+        cache_key = "scored_jobs"
+        if redis_client.exists(cache_key):
+            return json.loads(redis_client.get(cache_key))
+
         obj = s3.get_object(Bucket=BUCKET, Key="scored_jobs.json")
-        return json.loads(obj["Body"].read().decode("utf-8"))
+        relevance = json.loads(obj["Body"].read().decode("utf-8"))
+
+        redis_client.setex(cache_key, CACHE_TTL, json.dumps(relevance))
+        return relevance
     except s3.exceptions.NoSuchKey:
         return {}
     except Exception:
@@ -76,3 +111,6 @@ def update_like(job_ref: str, feedback: str):
         Body=json.dumps(existing_data, indent=2),
         ContentType="application/json",
     )
+
+    # MAJ du cache Redis
+    redis_client.setex("job_likes", CACHE_TTL, json.dumps(existing_data))
